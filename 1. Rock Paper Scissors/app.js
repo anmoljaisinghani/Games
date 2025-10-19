e09f356f302636bb5eacf0b10db16927b4222423
Anmol Jaisinghani <anmoljaisinghani7@gmail.com>
2025-10-20 00:12:45 +0530

    Project update: upload to GitHub

---
+++ 1. Rock Paper Scissors/app.js
@@ -0,0 +1,96 @@
+// Cache the audio elements
+const winSound = document.getElementById('win-sound');
+const loseSound = document.getElementById('lose-sound');
+const drawSound = document.getElementById('draw-sound');
+
+// Cache the DOM elements (storing for future use) & reset everything to 0 value
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById('user-score');
+const computerScore_span = document.getElementById('computer-score');
+const scoreBoard_div = document.querySelector('.score-board');
+const result_div = document.querySelector('.result');
+const rock_div = document.getElementById('rock');
+const paper_div = document.getElementById('paper');
+const scissors_div = document.getElementById('scissors');
+
+// Set up the core function for the computer that will use Math.random to loop through an array and return that value
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+// Similar to convert case but just takes lowercase and replaces with title case
+function convertCase(anythingIwant) {
+    if (anythingIwant === 'paper') return 'Paper';
+    if (anythingIwant === 'scissors') return 'Scissors';
+    return 'Rock';
+}
+
+// Winning Condition - this handles what happens when the user clicks one of the choices where the value is then passed through as a parameter
+function win(user, computer) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    const userName = ' (user)'.fontsize(3).sup();
+    const compName = ' (comp)'.fontsize(3).sup();
+    result_div.innerHTML = `<p>${convertCase(user)}${userName} beats ${convertCase(computer)}${compName}. You win!</p>`;
+    const roundStatus = document.getElementById(user);
+    roundStatus.classList.add('winningStyles');
+    setTimeout(() => roundStatus.classList.remove('winningStyles'), 300);
+    winSound.play();  // Play win sound
+}
+
+// Losing Condition - this handles what happens when the user clicks one of the choices where the value is then passed through as a parameter
+function loses(user, computer) {
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    const userName = ' (user)'.fontsize(3).sup();
+    const compName = ' (comp)'.fontsize(3).sup();
+    result_div.innerHTML = `<p>${convertCase(computer)}${compName} beats ${convertCase(user)}${userName}. You lose!</p>`;
+    const roundStatus = document.getElementById(user);
+    roundStatus.classList.add('losingStyles');
+    setTimeout(() => roundStatus.classList.remove('losingStyles'), 300);
+    loseSound.play();  // Play lose sound
+}
+
+// Draw Condition - this handles what happens when the user clicks one of the choices where the value is then passed through as a parameter
+function draw(user, computer) {
+    const userName = ' (user)'.fontsize(3).sup();
+    const compName = ' (comp)'.fontsize(3).sup();
+    result_div.innerHTML = `<p>It was a draw! You both chose ${convertCase(user)}</p>`;
+    const roundStatus = document.getElementById(user);
+    roundStatus.classList.add('drawStyles');
+    setTimeout(() => roundStatus.classList.remove('drawStyles'), 300);
+    drawSound.play();  // Play draw sound
+}
+
+// The core game functions that set up and determine the game's actual logic aka paper beats rock etc
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case 'paperrock':
+        case 'rockscissors':
+        case 'scissorspaper':
+            win(userChoice, computerChoice);
+            break;
+        case 'rockpaper':
+        case 'scissorsrock':
+        case 'paperscissors':
+            loses(userChoice, computerChoice);
+            break;
+        case 'rockrock':
+        case 'scissorsscissors':
+        case 'paperpaper':
+            draw(userChoice, computerChoice);
+            break;
+    }
+}
+
+// This function creates and adds an event listener to the rock, paper, scissors HTML element and then passes the value of that element to the game function
+function main() {
+    rock_div.addEventListener('click', () => game('rock'));
+    paper_div.addEventListener('click', () => game('paper'));
+    scissors_div.addEventListener('click', () => game('scissors'));
+}
+main();
